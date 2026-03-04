3aa7a25f06c9dad9782647d8e931a35c5d89b6b0
Ahmed Ramadan <103392399+A7med267@users.noreply.github.com>
2026-03-04 22:46:19 +0200

    Connect to MongoDB Atlas

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-// Middleware
 app.use(express.json());
+app.use(cors()); 
 
-//  (Route)
+const dbURI = process.env.MONGO_URI;
+
+mongoose.connect(dbURI)
+    .then(() => {
+        console.log("Connected to MongoDB Atlas successfully!");
+    })
+    .catch((err) => {
+        console.error("Connection error:", err.message);
+    });
+
 app.get('/', (req, res) => {
     res.send('Welcome to my backend project, Hamoudi!');
 });
